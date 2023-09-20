1cb5924c888b2639c8d94a3f464f4089cdf63b4a
danalovecode <newlifedana55@gmail.com>
2023-09-19 19:42:14 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// You can now view hellocra in the browser.
+
+//   Local:            http://localhost:3009
+//   On Your Network:  http://192.168.0.15:3009
